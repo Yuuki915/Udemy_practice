28ea48a7a697eb3e2628d6203464d09dbf501b94
Yuuki915 <aso091335@gmail.com>
2021-11-24 11:51:13 -0800

    add script

---
+++ Jonas_JS/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Corder";
+let myCurrentJob = "Teature";
+
+let job1 = "programmer";
+let job2 = "teature";
+
+console.log(myFirstJob);
+
+true;
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teature";
+
+lastName = "Schmedtmann";
+console.log(lastName);
