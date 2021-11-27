4e5367aabceeb0384e671680e957a65af627b939
Yuuki915 <aso091335@gmail.com>
2021-11-27 14:48:53 -0800

    practiced

--- Jonas_JS/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Jonas_JS/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -94,6 +94,9 @@ console.log(typeof null);
 // const averageAge = (ageJonas + ageSarah) / 2;
 // console.log(ageJonas, ageSarah, averageAge);
 
+// ----------
+// cording challenge #2
+
 // test 2
 // const massMark = 78;
 // const heightMark = 1.69;
@@ -118,24 +121,132 @@ console.log(typeof null);
 
 // console.log(BMIMark, BMIJohn, markHigherBMI);
 
-const firstName = "Jonas";
-const job = "teature";
-const birthYear = 1991;
-const year = 2037;
+// ----------
 
-const jonas =
-  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
-console.log(jonas);
+// const firstName = "Jonas";
+// const job = "teature";
+// const birthYear = 1991;
+// const year = 2037;
 
-const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
-console.log(jonasNew);
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+// console.log(jonas);
 
-console.log(`Just a regular string...`);
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jonasNew);
 
-console.log("String with \n\
-multipule \n\
-lines");
+// console.log(`Just a regular string...`);
 
-console.log(`String
-multipule
-lines`);
+// console.log("String with \n\
+// multipule \n\
+// lines");
+
+// console.log(`String
+// multipule
+// lines`);
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 2012;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+// ----------
+// cording challenge #2
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+// ----------
+// // type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
+
+// let n = "1" + 1;   // '11'
+// n = n - 1;
+// console.log(n);
+
+// ----------
+//  falsy values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// ----------
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favorite = Number(prompt("What's yout favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  // '23' == 23
+  console.log("Cool! 23 is an amazig number!");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number");
+} else if (favorite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favorite !== 23) console.log("Why not 23?");
+
+// ----------
