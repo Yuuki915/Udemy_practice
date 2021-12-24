40e843da691e434655ad1cbd6845a417823e020c
Yuuki915 <aso091335@gmail.com>
2021-12-24 13:12:04 -0800

    -72

--- Jonas_JS/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ Jonas_JS/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -120,14 +120,21 @@
 
 // Cording Challenge #1
 
-const data1 = [17, 21, 23];
-const data2 = [12, 5, -5, 0, 4];
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
 
-const printForecast = function (arr) {
-  let str = "";
-  for (let i = 0; i < arr.length; i++) {
-    str += `${arr[i]}ºC in ${i + 1} days ... `;
-  }
-  console.log("... " + str);
-};
-printForecast(data1);
+// const printForecast = function (arr) {
+//   let str = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}ºC in ${i + 1} days ... `;
+//   }
+//   console.log("... " + str);
+// };
+// printForecast(data1);
+
+// ----------
+
+// HTML は とばしました
+
+// ----------
+//
--- Jonas_JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Jonas_JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// ----------
+// 70
+// console.log(document.querySelector('.message').textContent);
+
+// ----------
+// 72
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+// ----------
+// 72
