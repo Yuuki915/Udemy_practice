16ad1a70c4841f12fbe7e6b5f116a6cddabb6876
Yuuki915 <aso091335@gmail.com>
2021-12-26 14:45:54 -0800

    -78

--- Jonas_JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Jonas_JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -17,11 +17,16 @@
 // console.log(document.querySelector('.guess').value);
 
 // ----------
-// 73
+// 73 -
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+
+const displayMesssage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,35 +34,87 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    // document.querySelector('.message').textContent = 'No number!';
+    displayMesssage('No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
-
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMesssage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60d347';
-
     document.querySelector('.number').style.width = '30rem';
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+      textContent = highscore;
     }
-    // When guess is too low
-  } else if (guess < secretNumber) {
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      //   document.querySelector('.message').textContent =
+      // guess > secretNumber ? 'Too high!' : 'Too low!';
+      displayMesssage(guess > secretNumber ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      //   document.querySelector('.message').textContent = 'You lost the game!';
+      displayMesssage('You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  //     // When guess is too high
+  //   } else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = 'Too high!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //     // When guess is too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = 'Too low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
 });
+
+// ----------
+// 76
+
+// Cording Challenge #1
+
+// mine
+// document.querySelector('.again').addEventListener('click', function () {
+//   document.querySelector('.score').textContent = 20;
+//   document.querySelector('.guess');
+//   document.querySelector('.message').textContent = 'Start guessing...';
+//   document.querySelector('.number').textContent = secretNumber;
+//   document.querySelector('body').style.backgroundColor = '#222';
+// });
+
+// jonas'
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  //   document.querySelector('.message').textContent = 'Start guessing...';
+  displayMesssage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+// ----------
+// 77
